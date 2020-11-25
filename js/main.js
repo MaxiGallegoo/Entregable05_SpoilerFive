8a269cf0dac7ca36431a97216880f107f3f33685
Gero Mendy <geromendy@gmail.com>
2020-11-25 08:54:26 -0300

    Mergeado JS

--- js/main.js
+++ js/main.js
@@ -35,15 +35,21 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#js-search").addEventListener("click", function () {
         let param1 = "../html/busqueda.html";
         let param2 = document.querySelector("#js-contenido");
-        let param3 = function () {  let clave=document.querySelector("#js-input-search").value;
-                                    cargarBusqueda(clave) };
+        let param3 = function () {
+            let clave = document.querySelector("#js-input-search").value;
+            cargarBusqueda(clave)
+        };
         loadPage(param1, param2, param3);
     });
-    document.querySelector("#js-input-search").addEventListener("input", function(){let param1 = "../html/busqueda.html";
-    let param2 = document.querySelector("#js-contenido");
-    let param3 = function () {  let clave=document.querySelector("#js-input-search").value;
-                                cargarBusqueda(clave) };
-                                loadPage(param1, param2, param3);});
+    document.querySelector("#js-input-search").addEventListener("input", function () {
+        let param1 = "../html/busqueda.html";
+        let param2 = document.querySelector("#js-contenido");
+        let param3 = function () {
+            let clave = document.querySelector("#js-input-search").value;
+            cargarBusqueda(clave)
+        };
+        loadPage(param1, param2, param3);
+    });
     //FIN LISTENERS
 
     function cargarFavBar() {
@@ -106,7 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function cargarDatosPaginaLista(lista_reproduccion) {
         if (!lista_reproduccion) return;
         cargarTablaDeCancionesParaPaginaPlaylist(lista_reproduccion.canciones);
-        let divValoracion=document.querySelector("#js-valoracion-playlist");
+        let divValoracion = document.querySelector("#js-valoracion-playlist");
         dibujarEstrellasPlaylist(divValoracion, lista_reproduccion);
         document.querySelector("#js-playlist_card").src = lista_reproduccion.imageURL;
         let arr_parrafos = document.querySelectorAll(".js-playlist_data");
@@ -130,14 +136,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
             let boton_play = document.createElement("button");
             let image_play = document.createElement("img");
-            image_play.src="../img/Iconos/Base/botones_armados/Button_Play.svg";
+            image_play.src = "../img/Iconos/Base/botones_armados/Button_Play.svg";
             boton_play.appendChild(image_play);
 
             let boton_add = document.createElement("button");
             let image_add = document.createElement("img");
-            image_add.src="../img/Iconos/Base/botones_armados/Button_Add.svg";
+            image_add.src = "../img/Iconos/Base/botones_armados/Button_Add.svg";
             boton_add.appendChild(image_add);
-            
+
             boton_play.addEventListener("click", function () {
                 player.reproducir(
                     audios[element]);
@@ -146,7 +152,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             let fila = document.createElement("tr");
             let nom_cancion = document.createElement("td");
-            
+
 
             nom_cancion.appendChild(boton_add);
             nom_cancion.innerHTML += cancion.nombre_cancion;
@@ -172,153 +178,153 @@ document.addEventListener("DOMContentLoaded", () => {
             tabla.appendChild(fila);
         });
     }
-    function cargarBusqueda(clave){
-        let listaSalida=listaBusqueda(clave);
-        let divSalida=document.querySelector("#js-resultados");
-        divSalida.innerHTML="";
-        if (listaSalida.length>0){
+    function cargarBusqueda(clave) {
+        let listaSalida = listaBusqueda(clave);
+        let divSalida = document.querySelector("#js-resultados");
+        divSalida.innerHTML = "";
+        if (listaSalida.length > 0) {
             listaSalida.forEach(element => {
-                let divCard=document.createElement("div");
-                    divCard.classList.add("individual_card");
-                let imgcard=document.createElement("img");
-                    imgcard.src=element.imagen;
+                let divCard = document.createElement("div");
+                divCard.classList.add("individual_card");
+                let imgcard = document.createElement("img");
+                imgcard.src = element.imagen;
                 divCard.append(imgcard);
                 divCard.append(document.createElement("div"));
-                let btn1=document.createElement("button");
-                    let img1=document.createElement("img");
-                    img1.src="../img/Iconos/Base/botones_armados/Button_Add.svg";
-                    btn1.append(img1);
+                let btn1 = document.createElement("button");
+                let img1 = document.createElement("img");
+                img1.src = "../img/Iconos/Base/botones_armados/Button_Add.svg";
+                btn1.append(img1);
                 divCard.append(btn1);
-                let titulo=document.createElement("p");
-                    titulo.innerHTML=element.nombre_cancion;
+                let titulo = document.createElement("p");
+                titulo.innerHTML = element.nombre_cancion;
                 divCard.append(titulo);
-                let btn2=document.createElement("button");
-                    let img2=document.createElement("img");
-                        img2.src="../img/Iconos/Base/botones_armados/Button_Play.svg";
-                    btn2.append(img2);
+                let btn2 = document.createElement("button");
+                let img2 = document.createElement("img");
+                img2.src = "../img/Iconos/Base/botones_armados/Button_Play.svg";
+                btn2.append(img2);
                 divCard.append(btn2);
 
                 divSalida.append(divCard);
             });
         }
-        else{
-            let pe=document.createElement("p");
-            pe.innerHTML="No se encontraron resultados";
+        else {
+            let pe = document.createElement("p");
+            pe.innerHTML = "No se encontraron resultados";
             divSalida.append(pe);
         }
     }
 
-    function dibujarEstrellasPlaylist(div, lista){
-        let estrella1=document.createElement("IMG");
-        if (lista.valoracion>=1){
-            estrella1.src="../img/Iconos/Base/EstrellaLlena.svg";
-            if(lista.valorada)
-                estrella1.src="../img/Iconos/Base/EstrellaLlenaDorada.svg";
+    function dibujarEstrellasPlaylist(div, lista) {
+        let estrella1 = document.createElement("IMG");
+        if (lista.valoracion >= 1) {
+            estrella1.src = "../img/Iconos/Base/EstrellaLlena.svg";
+            if (lista.valorada)
+                estrella1.src = "../img/Iconos/Base/EstrellaLlenaDorada.svg";
         }
         else
-            estrella1.src="../img/Iconos/Base/Estrella.svg";
-        let estrella2=document.createElement("IMG");
-        if (lista.valoracion>=2){
-            estrella2.src="../img/Iconos/Base/EstrellaLlena.svg";
-            if(lista.valorada)
-                estrella2.src="../img/Iconos/Base/EstrellaLlenaDorada.svg";
+            estrella1.src = "../img/Iconos/Base/Estrella.svg";
+        let estrella2 = document.createElement("IMG");
+        if (lista.valoracion >= 2) {
+            estrella2.src = "../img/Iconos/Base/EstrellaLlena.svg";
+            if (lista.valorada)
+                estrella2.src = "../img/Iconos/Base/EstrellaLlenaDorada.svg";
         }
-            else
-                estrella2.src="../img/Iconos/Base/Estrella.svg";
-        let estrella3=document.createElement("IMG");
-        if (lista.valoracion>=3){
-            estrella3.src="../img/Iconos/Base/EstrellaLlena.svg";
-            if(lista.valorada)
-                estrella3.src="../img/Iconos/Base/EstrellaLlenaDorada.svg";
+        else
+            estrella2.src = "../img/Iconos/Base/Estrella.svg";
+        let estrella3 = document.createElement("IMG");
+        if (lista.valoracion >= 3) {
+            estrella3.src = "../img/Iconos/Base/EstrellaLlena.svg";
+            if (lista.valorada)
+                estrella3.src = "../img/Iconos/Base/EstrellaLlenaDorada.svg";
         }
-            else
-                estrella3.src="../img/Iconos/Base/Estrella.svg";
-        let estrella4=document.createElement("IMG");
-        if (lista.valoracion>=4){
-            estrella4.src="../img/Iconos/Base/EstrellaLlena.svg";
-            if(lista.valorada)
-                estrella4.src="../img/Iconos/Base/EstrellaLlenaDorada.svg";
+        else
+            estrella3.src = "../img/Iconos/Base/Estrella.svg";
+        let estrella4 = document.createElement("IMG");
+        if (lista.valoracion >= 4) {
+            estrella4.src = "../img/Iconos/Base/EstrellaLlena.svg";
+            if (lista.valorada)
+                estrella4.src = "../img/Iconos/Base/EstrellaLlenaDorada.svg";
         }
-            else
-                estrella4.src="../img/Iconos/Base/Estrella.svg";
-        let estrella5=document.createElement("IMG");
-        if (lista.valoracion==5){
-            estrella5.src="../img/Iconos/Base/EstrellaLlena.svg";
-            if(lista.valorada)
-                estrella5.src="../img/Iconos/Base/EstrellaLlenaDorada.svg";
+        else
+            estrella4.src = "../img/Iconos/Base/Estrella.svg";
+        let estrella5 = document.createElement("IMG");
+        if (lista.valoracion == 5) {
+            estrella5.src = "../img/Iconos/Base/EstrellaLlena.svg";
+            if (lista.valorada)
+                estrella5.src = "../img/Iconos/Base/EstrellaLlenaDorada.svg";
         }
-            else
-                estrella5.src="../img/Iconos/Base/Estrella.svg";
-        if (div!=null)
-            div.innerHTML="";
+        else
+            estrella5.src = "../img/Iconos/Base/Estrella.svg";
+        if (div != null)
+            div.innerHTML = "";
         div.appendChild(estrella1);
         div.appendChild(estrella2);
         div.appendChild(estrella3);
         div.appendChild(estrella4);
         div.appendChild(estrella5);
-        
+
         //COMENTAR HASTA EL FINAL SI ROMPE
-        let f0=function(){
-                for (let i=0;i<div.children.length;i++){
-                    div.children[i].src="../img/Iconos/Base/Estrella.svg";
-                }
-                div.children[0].src="../img/Iconos/Base/EstrellaLlena.svg";
+        let f0 = function () {
+            for (let i = 0; i < div.children.length; i++) {
+                div.children[i].src = "../img/Iconos/Base/Estrella.svg";
             }
-        let f01=function(){
-                lista.valorada=true;
-                lista.valoracion=1;
-            }
-        let f1=function(){
-                for (let i=0;i<div.children.length;i++){
-                    div.children[i].src="../img/Iconos/Base/Estrella.svg";
-                }
-                div.children[0].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[1].src="../img/Iconos/Base/EstrellaLlena.svg";
-            }
-        let f11=function(){
-                lista.valorada=true;
-                lista.valoracion=2;
-            }
-        let f2=function(){
-                for (let i=0;i<div.children.length;i++){
-                    div.children[i].src="../img/Iconos/Base/Estrella.svg";
-                }
-                div.children[0].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[1].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[2].src="../img/Iconos/Base/EstrellaLlena.svg";
-            }
-        let f21=function(){
-            lista.valorada=true;
-            lista.valoracion=3;
+            div.children[0].src = "../img/Iconos/Base/EstrellaLlena.svg";
         }
-        let f3=function(){
-                for (let i=0;i<div.children.length;i++){
-                    div.children[i].src="../img/Iconos/Base/Estrella.svg";
-                }
-                div.children[0].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[1].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[2].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[3].src="../img/Iconos/Base/EstrellaLlena.svg";
+        let f01 = function () {
+            lista.valorada = true;
+            lista.valoracion = 1;
+        }
+        let f1 = function () {
+            for (let i = 0; i < div.children.length; i++) {
+                div.children[i].src = "../img/Iconos/Base/Estrella.svg";
             }
-        let f31=function(){
-                lista.valorada=true;
-                lista.valoracion=4;
+            div.children[0].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[1].src = "../img/Iconos/Base/EstrellaLlena.svg";
+        }
+        let f11 = function () {
+            lista.valorada = true;
+            lista.valoracion = 2;
+        }
+        let f2 = function () {
+            for (let i = 0; i < div.children.length; i++) {
+                div.children[i].src = "../img/Iconos/Base/Estrella.svg";
             }
-        let f4=function(){
-                for (let i=0;i<div.children.length;i++){
-                    div.children[i].src="../img/Iconos/Base/Estrella.svg";
-                }
-                div.children[0].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[1].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[2].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[3].src="../img/Iconos/Base/EstrellaLlena.svg";
-                div.children[4].src="../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[0].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[1].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[2].src = "../img/Iconos/Base/EstrellaLlena.svg";
+        }
+        let f21 = function () {
+            lista.valorada = true;
+            lista.valoracion = 3;
+        }
+        let f3 = function () {
+            for (let i = 0; i < div.children.length; i++) {
+                div.children[i].src = "../img/Iconos/Base/Estrella.svg";
             }
-        let f41=function(){
-                lista.valorada=true;
-                lista.valoracion=5;
+            div.children[0].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[1].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[2].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[3].src = "../img/Iconos/Base/EstrellaLlena.svg";
+        }
+        let f31 = function () {
+            lista.valorada = true;
+            lista.valoracion = 4;
+        }
+        let f4 = function () {
+            for (let i = 0; i < div.children.length; i++) {
+                div.children[i].src = "../img/Iconos/Base/Estrella.svg";
             }
-        let x=function(){dibujarEstrellasPlaylist(div, lista)}
+            div.children[0].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[1].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[2].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[3].src = "../img/Iconos/Base/EstrellaLlena.svg";
+            div.children[4].src = "../img/Iconos/Base/EstrellaLlena.svg";
+        }
+        let f41 = function () {
+            lista.valorada = true;
+            lista.valoracion = 5;
+        }
+        let x = function () { dibujarEstrellasPlaylist(div, lista) }
         div.removeEventListener("mouseleave", x.bind(this));
         div.children[0].removeEventListener("mouseover", f0);
         div.children[0].removeEventListener("click", f01);
@@ -330,7 +336,7 @@ document.addEventListener("DOMContentLoaded", () => {
         div.children[3].removeEventListener("click", f31);
         div.children[4].removeEventListener("mouseover", f4);
         div.children[4].removeEventListener("click", f41);
-        if (!lista.valorada){
+        if (!lista.valorada) {
             div.children[0].addEventListener("mouseover", f0);
             div.children[0].addEventListener("click", f01);
             div.children[1].addEventListener("mouseover", f1);
@@ -341,7 +347,7 @@ document.addEventListener("DOMContentLoaded", () => {
             div.children[3].addEventListener("click", f31);
             div.children[4].addEventListener("mouseover", f4);
             div.children[4].addEventListener("click", f41);
-            let x=function(){dibujarEstrellasPlaylist(div, lista)}
+            let x = function () { dibujarEstrellasPlaylist(div, lista) }
             div.addEventListener("mouseleave", x.bind(this));
         }
     }
