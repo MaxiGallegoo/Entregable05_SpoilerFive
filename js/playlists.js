63406bfe2a056a8e08cf7e019871758056e3cab7
Gero Mendy <geromendy@gmail.com>
2020-11-26 11:14:20 -0300

    Preparado DB para ultimas valoraciones

--- js/playlists.js
+++ js/playlists.js
@@ -12,7 +12,28 @@ let listas = [
         "valoracion": 4,
         "valorada": false,
         "cant_valoraciones": 10,
-        "ultimas_valoraciones": [2, 2, 4, 3, 5],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_reproducciones": 9,
         "canciones": [172, 175, 180, 182, 183, 185, 187],
         "imagen": "../img/Card/Playlist/podcasts.png"
@@ -22,7 +43,28 @@ let listas = [
         "valoracion": 2,
         "valorada": false,
         "cant_valoraciones": 10,
-        "ultimas_valoraciones": [2, 1, 3, 1, 5],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_reproducciones": 15000,
         "canciones": [85, 86, 87, 88, 89, 90],
         "imagen": "../img/Card/Playlist/coding.png"
@@ -31,7 +73,28 @@ let listas = [
         "nombre": "Nacional",
         "valoracion": 4,
         "valorada": false,
-        "ultimas_valoraciones": [4, 3, 4, 5, 2],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_valoraciones": 10,
         "cant_reproducciones": 75187000,//75M
         "canciones": [19, 23, 31, 33, 60, 67, 68, 92, 95],
@@ -41,7 +104,28 @@ let listas = [
         "nombre": "Lo mejor del Rock",
         "valoracion": 4,
         "valorada": false,
-        "ultimas_valoraciones": [5, 2, 4, 3, 4],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_valoraciones": 10,
         "cant_reproducciones": 875187000,//875M
         "canciones": [30, 8, 12, 29, 6, 3, 13, 7, 11, 31, 10, 5, 1, 32, 2, 4, 9],
@@ -55,7 +139,28 @@ let listas_recomendadas = [
         "valoracion": 4,
         "valorada": false,
         "cant_valoraciones": 10,
-        "ultimas_valoraciones": [2, 2, 4, 3, 5],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_reproducciones": 9,
         "canciones": [33, 115, 150, 29, 58, 59, 110, 121, 77, 149, 28, 62, 118, 34, 111, 147, 30, 78, 75],// 28-34 , 57-78 , 110-121 , 147-150
         "imagen": "../img/Card/Artistas_Song/La_renga.jpg"
@@ -66,7 +171,28 @@ let listas_recomendadas = [
         "valoracion": 4,
         "valorada": false,
         "cant_valoraciones": 1588,
-        "ultimas_valoraciones": [2, 2, 4, 3, 5],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_reproducciones": 9,
         "canciones": [31, 34, 28, 32, 29, 33, 30],//28-34
         "imagen": "../img/Card/Artistas_Song/Cerati.jpg"
@@ -77,7 +203,28 @@ let listas_recomendadas = [
         "valoracion": 4,
         "valorada": false,
         "cant_valoraciones": 10,
-        "ultimas_valoraciones": [2, 2, 4, 3, 5],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_reproducciones": 9,
         "canciones": [172, 180, 182, 183, 185, 187],
         "imagen": "../img/Card/Playlist/podcasts.png"
@@ -87,7 +234,28 @@ let listas_recomendadas = [
         "valoracion": 4,
         "valorada": false,
         "cant_valoraciones": 10,
-        "ultimas_valoraciones": [2, 2, 4, 3, 5],
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_reproducciones": 9,
         "canciones": [12, 123, 137, 4, 164, 154, 1, 48, 55, 91, 125, 128, 138, 166, 165, 9, 2, 90, 124, 49],    //1-12 , 45-56 , 86-91 , 123-128 , 135-139 , 151-166
         "imagen": "../img/Card/Playlist/podcasts.png"
@@ -98,7 +266,28 @@ let listas_recomendadas = [
         "valoracion": 4,
         "valorada": false,
         "cant_valoraciones": 10,
-        "ultimas_valoraciones": [20, 22, 36, 23, 40, 25, 35, 37, 21, 38, 24, 39, 26, 27], //35-43 , 20-27
+        "ultimas_valoraciones": [
+            {
+                "user": "Marcos",
+                "val": 5
+            },
+            {
+                "user": "Mirta",
+                "val": 4
+            },
+            {
+                "user": "Juan",
+                "val": 2
+            },
+            {
+                "user": "Eduardo",
+                "val": 3
+            },
+            {
+                "user": "Miguel",
+                "val": 3
+            }
+        ],
         "cant_reproducciones": 9,
         "canciones": [172, 175, 180, 182, 183, 185, 187],
         "imagen": "../img/Card/Playlist/podcasts.png"
